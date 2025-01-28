209a7421e5ce9adc77f8116426088cabf1e79df8
Rheina1999 <rheinawardiana317@gmail.com>
2025-01-28 20:58:08 +0700

    Add files via upload

---
+++ data.js
@@ -0,0 +1,229 @@
+
+const teams = [ 
+    {
+        "name": "Muhammad Khaidir Fahram",
+        "email": "khaidir@fahram.com",
+        "image": "https://fahram.com//img/profile-photos/jzZj0jPuEKTWwY9a8lxo7pQhA29zxFpVQd7xl4BZ.jpg",
+        
+        "link" : [
+            {
+                "name" : "instagram",
+                "link" : "https://instagram.com/khaidir.fahram"
+            },
+            {
+                "name" : "facebook",
+                "link" : "https://facebook.com/khaidir.fahram"
+            },
+
+            {
+                "name" : "globe",
+                "link" : "https://fahram.com/"
+            }
+        ]
+    },
+
+    {
+        "name": "Achmad Maulana Ichsan",
+        "email": "ichsanachmadmaulana@gmail.com",
+        "image": "https://tse4.mm.bing.net/th?id=OIP.g6sTdGfbyN2DffeFBszqqQAAAA&pid=Api&P=0&h=22",
+        "link" : [
+            {
+                "name" : "instagram",
+                "link" : "https://instagram.com/a.iichsan"
+            },
+
+            {
+                "name" : "globe",
+                "link" : "https://icsan.my.id/"
+            }
+        ]
+    },
+
+    {
+        "name": "Marcella Setyaning Putri",
+        "email": "msetyaningputri@gmail.com",
+        "image": "https://tse4.mm.bing.net/th?id=OIP.g6sTdGfbyN2DffeFBszqqQAAAA&pid=Api&P=0&h=220",
+        "link" : [
+            {
+                "name" : "instagram",
+                "link" : "https://www.instagram.com/mqrcelllaaaa?igsh=MTk5aWhyYXMybXN5bA== "
+            },
+
+            {
+                "name" : "globe",
+                "link" : "https://marcellasetyaningputri.my.id/"
+            }
+        ]
+    },
+
+    {
+        "name": "Muhammad Fikri",
+        "email": "mfikrif06@gmail.com",
+        "image": "https://tse4.mm.bing.net/th?id=OIP.g6sTdGfbyN2DffeFBszqqQAAAA&pid=Api&P=0&h=220",
+        "link" : [
+            {
+                "name" : "instagram",
+                "link" : "https://www.instagram.com/m_f1kr1",
+            },
+
+            {
+                "name" : "globe",
+                "link" : "https://fikriy.my.id/"
+            }
+        ]
+    },
+
+    {
+        "name": "Muhammad Rivanda ",
+        "email": "ramdhanirivanda@gmail.com ",
+        "image": "https://tse4.mm.bing.net/th?id=OIP.g6sTdGfbyN2DffeFBszqqQAAAA&pid=Api&P=0&h=220",
+        "link" : [
+            {
+                "name" : "instagram",
+                "link" : "https://www.instagram.com/ramdhanirivanda/?igsh=ZjM4ODNla2VmcG1j"
+            },
+
+            {
+                "name" : "globe",
+                "link" : "https://marcellasetyaningputri.my.id/"
+            }
+        ]
+    },
+
+    {
+        "name": "Naufal",
+        "email": "naufallaudza194@gmail.com",
+        "image": "https://tse4.mm.bing.net/th?id=OIP.g6sTdGfbyN2DffeFBszqqQAAAA&pid=Api&P=0&h=220",
+        "link" : [
+            {
+                "name" : "instagram",
+                "link" : "https://www.instagram.com/naufalaudzaa"
+            },
+
+            {
+                "name" : "globe",
+                "link" : "https://naufal19.my.id/"
+            }
+        ]
+    },
+
+    {
+        "name": "Rozak",
+        "email": "rozakalbaidawi@gmail.com",
+        "image": "https://tse4.mm.bing.net/th?id=OIP.g6sTdGfbyN2DffeFBszqqQAAAA&pid=Api&P=0&h=220",
+        "link" : [
+            {
+                "name" : "instagram",
+                "link" : "https://www.instagram.com/_rzkalbdwi/"
+            },
+
+            {
+                "name" : "globe",
+                "link" : "https://naufal19.my.id/"
+            }
+        ]
+    },
+
+    {
+        "name": "Sandi",
+        "email": "sandi@gmail.com",
+        "image": "https://tse4.mm.bing.net/th?id=OIP.g6sTdGfbyN2DffeFBszqqQAAAA&pid=Api&P=0&h=220",
+        "link" : [
+            {
+                "name" : "instagram",
+                "link" : ""
+            },
+            {
+                "name" : "globe",
+                "link" : "https://sandinourman.my.id/"
+            }
+        ]
+    },
+
+    {
+        "name": "Hanif Dwi Novario",
+        "email": "hanifdn05@gmail.com",
+        "image": "https://tse4.mm.bing.net/th?id=OIP.g6sTdGfbyN2DffeFBszqqQAAAA&pid=Api&P=0&h=22",
+        "link" : [
+            {
+                "name" : "instagram",
+                "link" : "https://instagram.com/hnfdwnvr/"
+            },
+            
+            {
+                "name" : "globe",
+                "link" : "https://hanifdwi.my.id/"
+            }
+        ]
+    },
+
+    {
+        "name": "Abdul Rahman",
+        "email": "fahganindo12@gmail.com",
+        "image": "https://tse4.mm.bing.net/th?id=OIP.g6sTdGfbyN2DffeFBszqqQAAAA&pid=Api&P=0&h=220",
+        "link" : [
+            {
+                "name" : "instagram",
+                "link" : "https://www.instagram.com/arafah.abdulrahman_/"
+            },
+            
+            {
+                "name" : "globe",
+                "link" : "https://fahganindo4life.my.id/"
+            }
+        ]
+    },
+
+    {
+        "name": "Rheina Wardiana Hafiroh",
+        "email": "rheinawardiana317@gmail.com ",
+        "image": "me.jpg",
+        "link" : [
+            {
+                "name" : "instagram",
+                "link" : "https://www.instagram.com/na_rrrrr44/"
+            },
+            
+            {
+                "name" : "globe",
+                "link" : "https://rheinawardianahafiroh.my.id/"
+            }
+        ]
+    },
+
+    {
+        "name": "Siti Aisyah",
+        "email": "siti.aisyah.2021@gmail.com",
+        "image": "https://tse4.mm.bing.net/th?id=OIP.g6sTdGfbyN2DffeFBszqqQAAAA&pid=Api&P=0&h=220",
+        "link" : [
+            {
+                "name" : "instagram",
+                "link" : ""
+            },
+            
+            {
+                "name" : "globe",
+                "link" : "https://kaisyah.my.id/"
+            }
+        ]
+    },
+
+    {
+        "name": "Fredo",
+        "email": "Fredopradana72@gmail.com",
+        "image": "https://tse4.mm.bing.net/th?id=OIP.g6sTdGfbyN2DffeFBszqqQAAAA&pid=Api&P=0&h=220",
+        "link" : [
+            {
+                "name" : "instagram",
+                "link" : ""
+            },
+            
+            {
+                "name" : "globe",
+                "link" : "https://kaisyah.my.id/"
+            }
+        ]
+    },
+
+    
+]
